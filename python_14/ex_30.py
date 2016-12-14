4c8b37e48808e15d61067a793e55e732a48f56bd
haiyuancheng <haiyuancheng@hpe.com>
2016-12-15 00:35:53 +0800

    practise python

---
+++ python_14/ex_30.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#-*- coding: UTF-8 -*-
+#Accessing Elements of Lists 具体见ex_28.py 和你学到了什么中具体的list tuple dic练习
+
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+print bear
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print animals[0]
+print animals[2]
+print animals[0]
+print animals[2]
+print animals[4]
+print animals[1]
+print animals[5]
+print animals[3]
+
+#tuple 只有两个方法 count和index
+>>> names = ('Dave', 'Grace' , 'Jason')
+>>> names.count('Dave')
+1
+>>> names.index('Dave')
+0
